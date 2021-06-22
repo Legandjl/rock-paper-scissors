7675d95f6397c168770e973d9cc8574f619c769b
dorian <dorianlegan@gmail.com>
2021-06-22 08:41:25 +0100

    redone completely this morning

--- script.js
+++ script.js
@@ -1,168 +1,52 @@
-console.log("hello wddoo");
+
 let hands = ["Rock", "Paper", "Scissors"];
 let handsLogic = new Map();
-
+handsLogic.set("paper", "scissors");
+handsLogic.set("rock", "paper");
+handsLogic.set("scissors", "rock");
 
 
 function computerplay() {
     
     return (hands[getRandom()].toLowerCase()) + "yooo";
 
-    /*for (i = 0; i < hands.length; i++) {
-
-        console.log(hands[i]);
-        console.log(getRandom());
-    }
-
-    */  
 }
 
 
-function gameRound(playerselectiom, computerselection) {
 
-    switch(true) {
 
-        case(playerselectiom == computerselection):
+function gameRound(player, computer) {
+
+    if(player == computer) {
+
         console.log("draw")
-        break;
-
-        case(playerselectiom == "rock" || computerselection == "rock"):
-
-       
-
-        if (rockCheck(computerselection)) {
-
-            console.log(playerselectiom + " beats" + computerselection + " You win")
-        }
-
-        else {
-
-            console.log("reached")
-
-            console.log(computerselection + " beats" + playerselectiom + "you lose")
-        }
-
-        break;
+    }
 
 
 
-
-
-
-         case(playerselectiom == "paper"):
-
-
-
-        if (paperCheck(computerselection)) {
-
-            console.log(playerselectiom + " beats" + computerselection + " You win")
-        }
-
-        else {
-
-            console.log("reached")
-
-            console.log(computerselection + " beats" + playerselectiom + "you lose")
-        }
-
-         break;
-
-
-         case (computerselection == "scissors"):
-
-
-
-
-        if (scissorsCheck(computerselection)) {
-
-            console.log(playerselectiom + " beats" + computerselection + " You win")
-        }
-
-        else {
-
-            console.log("reached")
-
-            console.log(computerselection + " beats" + playerselectiom + "you lose")
-        }
-
-         break;
-
-
-         case (playerselectiom == "scissors"):
-
-         break;
-
-
-         case (computerselection == "scissors"):
-
-         break;
-
-
+    else if (player != handsLogic.get(computer)) {
 
         
-        
-       
-    }
-}
 
-function getRandom() {
-
-    return Math.floor((Math.random()* 3));
-}
+        console.log("you lose" + computer + "beats" + player);
 
 
-function giveWinner(handChoice) {
-
-    
-
-
-
-}
-
-
-function rockCheck(playerselectiom) {
-
-    if (playerselectiom == "paper") {
-
-        return false;
-    }
-
-    else if (playerselectiom == "scissors") {
-
-        return true;
-    }
-
-
-}
-
-function paperCheck(selection) {
-
-    if (selection == "rock") {
-
-        return true;
     }
 
     else {
 
-        return false;
+        console.log("you win" + player + "beats" + computer);
     }
+
+
+    console.log(handsLogic.get(player))
 }
 
+gameRound("paper", "scissors");
 
-function scissorsCheck(selection) {
 
-    if (selection == "paper") {
-
-        return true;
-    }
-
-    else {
-
-        return false;
-    }
-}
+   
 
 
 
 
-console.log(gameRound("paper", "scissors"));
